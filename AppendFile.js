efe90bd52bd70bbd3880b61d3f5017d9cd8bddf5
Nurudeen Mohammed <nurahuloty@gmail.com>
2024-12-02 15:53:24 +0100

    Implemented game wining logic

--- AppendFile.js
+++ AppendFile.js
@@ -1,24 +1,39 @@
-import { winLogic } from "./AppendFile2";
-let PlayerOne = "X";
-let PlayerTwo = "Y";
+import { winLogic } from "./AppendFile2.js";
+const PlayerX = "X";
+const PlayerY = "Y";
 let PlayerHasPlayed = false;
+const Choices = document.querySelectorAll("#box");
 
-
-let choices = document.querySelectorAll("#box");
-
-choices.forEach((choice) => {
+Choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
+    // Return after first player makes choice
+    if (choice.innerHTML) return;
+    // state the value of the player
     if (PlayerHasPlayed) {
-      choice.innerHTML = PlayerTwo;
+      choice.innerHTML = PlayerY;
     } else {
-      choice.innerHTML = PlayerOne;
+      choice.innerHTML = PlayerX;
     }
-    // if player one has played, player two's turn
+    // decalre gameboard content and map tgem to their innerHtml
+    const Gameboard = Array.from(Choices).map((node) => node.innerHTML);
+
+    TestWin(Gameboard);
+
     PlayerHasPlayed = !PlayerHasPlayed;
-
-
-   
-  
   });
-
 });
+
+function TestWin(Gameboard) {
+  for (const logic of winLogic) {
+    const PlayerXWins = logic.every((index) => Gameboard[index - 1] == PlayerX);
+    if (PlayerXWins) {
+      alert("Player one has won");
+      break;
+    }
+    const PlayerYWins = logic.every((index) => Gameboard[index - 1] == PlayerY);
+    if (PlayerYWins) {
+      alert("Player TWO has won");
+      break;
+    }
+  }
+}
